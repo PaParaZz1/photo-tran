d2b6e71ba18bbc56a6596d18e2ce8efba348cbc8
xinleipan <xinleipan@berkeley.edu>
2019-03-20 22:15:43 -0700

    add pickle load

--- tsne.py
+++ tsne.py
@@ -1,13 +1,17 @@
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
-import cPickle as pickle
+#import cPickle as pickle
+import pickle
 
 
 def main(path):
-    feature = pickle.load(open(path))
-    print(type(feature))
+    feature = pickle.load(open(path, 'rb'), encoding='bytes')
+    print(feature.shape)
+
+    
 
 
 if __name__ == "__main__":
-    path = 'mnist_feature/'
+    #path = '~/icml2019/mnist_features/mmbk_0.4943844261876095.pkl'
+    path = '/home/xinlei/icml2019/mnist_features/mmbk_0.4943844261876095.pkl'
     main(path)
